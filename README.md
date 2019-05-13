8537e008ee4dd58428d4713931c17683ed393bd1
abrarShariar <shariarabrar@gmail.com>
2019-05-13 10:41:44 +0600

    Update contracts

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # eth-todo-app
+
+
+```
+> truffle migrate 
+// for deploying again
+> truffle migrate --reset
+
+> truffle console
+> toDoList = await ToDoList.deployed()
+> toDoList.taskCount()
+> toDoList.createTask("My New Task")
+> toDoList.tasks(1)
+```
--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -4,4 +4,27 @@ pragma solidity ^0.5.0;
 contract ToDoList {
     // state vars
     uint public taskCount = 0;
+
+    // struct
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    // called when intitialized
+    constructor() public {
+        createTask("Hack dapp");
+    }
+
+    function createTask (string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    function showTask (uint id) {
+        return tasks[id];
+    }
 }
